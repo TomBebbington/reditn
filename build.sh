4319f71c0df71a6ea6c5548c8da54a3edb3b455a
Tom Bebbington <tom@toms-laptop.(none)>
2013-03-07 22:22:33 +0000

    Updated for firefox

--- build.sh
+++ build.sh
@@ -4,13 +4,11 @@ cat info >> temp
 cat reditn.js >> temp
 mv temp reditn.js
 rm temp
+cp ./reditn.js ./firefox/content/
+zip -r firefox.xpi ./firefox/
 cp ./reditn.js ./chrome/
 cp ./icon.svg ./chrome/
-cd ./chrome
-zip -r ../chrome.zip ./*
-cd ..
+zip -r chrome.zip ./chrome/
 cp ./reditn.js ./opera/includes/
 cp ./icon.svg ./opera/
-cd ./opera
-zip -r ../opera.oex ./*
-cd ..
+zip -r opera.oex ./opera/
